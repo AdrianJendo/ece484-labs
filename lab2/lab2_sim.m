2fcac0c9cbcb3d5c080bde9752e30a48f9154598
Adrian Jendo <ajendo@uwaterloo.ca>
2023-10-29 18:03:32 -0400

    Initial Commit

---
+++ lab2/lab2_sim.m
@@ -0,0 +1,25 @@
+k1 = -2.054;
+tau = 0.0222;
+
+r = -16;
+i = 9;
+
+g1 = 1;
+g0 = abs(2*r);
+k = tau/k1 * (r^2 + i^2); % try j = 10, 12, 18
+
+s = tf('s');
+
+P = k1 / (s*(tau*s+1));
+C = k * (s + 1/tau) / (g1 * s + g0);
+
+T = feedback(P*C, 1);
+stepinfo(T)
+
+%% b
+f = 1000;
+D = c2d(C, 1/f);
+
+%% c
+[NUM,DEN]=tfdata(D,'v');
+
